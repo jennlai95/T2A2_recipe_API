4a518b051ca15cc7e885959c1f081f94561562b3
jennlai95 <jenniferlaai@gmail.com>
2023-07-16 15:07:50 +1000

    Create cli command for create, dropping tables and seeding users

--- src/models/user.py
+++ src/models/user.py
@@ -1,13 +1,14 @@
 from init import db, ma 
 
 #create User model
-class User(db.model):
+class User(db.Model):
     __tablename__ = 'users'
     
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     email = db.Column(db.String, nullable=False, unique=True)
     password = db.Column(db.String, nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
 
 #create User Schema
 class UserSchema(ma.Schema):
