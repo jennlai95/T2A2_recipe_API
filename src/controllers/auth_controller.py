252a32ddaf78f0192f515ad96657e311e30d4566
jennlai95 <jenniferlaai@gmail.com>
2023-07-16 15:42:09 +1000

    Create register route

---
+++ src/controllers/auth_controller.py
@@ -0,0 +1,32 @@
+from flask import Blueprint, request
+from init import db,bcrypt
+from models.user import User, user_schema, users_schema
+from flask_jwt_extended import create_access_token
+from sqlalchemy.exc import IntegrityError
+from psycopg2 import errorcodes
+
+auth_bp = Blueprint('auth',__name__, url_prefix='/auth')
+
+@auth_bp.route('/register', methods=['POST'])
+def auth_register():
+    try: 
+        body_data = request.get_json()
+        
+        # Create a new User model instance from the user info 
+        user = User()
+        user.name = body_data.get('name')
+        user.email = body_data.get('email')
+        if body_data.get('password'):
+            user.password = bcrypt.generate_password_hash(body_data.get('password')).decode('utf-8')
+        #Add the user to session
+        db.session.add(user)
+        #commit to add user to the database
+        db.session.commit()
+        #Respond to the client
+        return user_schema.dump(user), 201
+    except IntegrityError as err:
+        if err.orig.pgcode == errorcodes.UNIQUE_VIOLATION:
+            return {'error': 'Email Address already in user'}, 409
+        if err.orig.pgcode == errorcodes.NOT_NULL_VIOLATION:
+            return { 'error': f'The {err.orig.diag.column_name} is required' }, 409
+        
